5aa73d5a8885740ec7e319457016d981f4dd9521
ruslanzharkov <rus_basket_kg@inbox.ru>
2017-10-24 22:29:13 +0600

    Added beautiful render weather

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -24,8 +24,20 @@ export class WeatherDisplay extends Component {
       const weatherData = this.state.weatherData;
       if (!weatherData)
         return <div>Loading...</div>
+
+      const weather = weatherData.weather[0];
+      const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";  
       return (
-        <div>{JSON.stringify(weatherData)}</div>  
+        <div>
+          <h1>
+            {weather.main} in {weatherData.name}
+            <img src={iconUrl} alt={weatherData.description} />
+          </h1>
+          <p>Current: {weatherData.main.temp}°</p>
+          <p>High: {weatherData.main.temp_max}°</p>
+          <p>Low: {weatherData.main.temp_min}°</p>
+          <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+        </div>  
       );
     }
 }
