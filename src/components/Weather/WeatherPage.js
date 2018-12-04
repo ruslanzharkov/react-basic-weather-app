f1ccdc8ef6d21a53a1e4a4453356d094c404fa48
loki <rus_basket_kg@inbox.ru>
2018-12-04 23:43:15 +0600

    Not worked

--- src/components/Weather/WeatherPage.js
+++ src/components/Weather/WeatherPage.js
@@ -9,8 +9,49 @@ class SingleWeatherPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cityName: ''
+            cityName: '',
+            weatherChart: {
+                labels: [1,2,3,4,5],
+                datasets: {
+                    label:'Population',
+                    data: [],
+                    backgroundColor:[
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)'
+                    ]
+                }
+            },
+            data: []
         };
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log(nextProps.weather) 
+        if (nextProps.weather !== prevState.data) {
+            return {
+                weatherChart: {
+                    label:'Population',
+                    data: nextProps.weather,
+                    backgroundColor:[
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)'
+                    ]
+                },
+                data: nextProps.weather
+            }
+        }
+
+        return null;
     }
 
     getWeatherByCity = () => {
@@ -27,7 +68,9 @@ class SingleWeatherPage extends Component {
             <Input value={this.state.value} onChange={this.cityChange} className={'weather-input'}/>
             <Button onClick={this.getWeatherByCity} className={'weather-button'}>Search</Button>
             <div>
-                {console.log(this.props.weather)}
+                <Line
+                    data={this.state.weatherChart}
+                />
             </div>
           </div>
         )
